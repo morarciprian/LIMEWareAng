fb160cbe3fed45d30361ab8aacbf92ddba08c6a5
Ciprian Morar <ciprian.morar@gmail.com>
2015-09-17 15:39:41 +0300

    Adding AngujarJS support

---
+++ LIMEWareClient/LIMEWareClient/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(LIMEWareClient.Startup))]
+
+namespace LIMEWareClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
