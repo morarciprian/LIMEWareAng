fa7af2dc48fa56d6088a0c2b153e4fd7803a1891
Ciprian Morar <ciprian.morar@gmail.com>
2015-09-18 08:34:49 +0300

    Print the new parameter object.

---
+++ LIMEWareClient/LIMEWareClient/Controllers/ParameterController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LIMEWareClient.Controllers
+{
+    public class ParameterController : ApiController
+    {
+        // GET: api/Parameter
+        public IEnumerable<externalBalanceUpdateResponseActiveDataListDataBalance> Get()
+        {
+            var retValue = new externalBalanceUpdateResponseActiveDataListDataBalance[]
+            {
+                new externalBalanceUpdateResponseActiveDataListDataBalance {dataExpiry="1",    dataValue= 1241234,dataValueSpecified=true,sapcRetCode=1,sapcRetCodeSpecified=true},
+                new externalBalanceUpdateResponseActiveDataListDataBalance {dataExpiry="2",    dataValue= 121563456,dataValueSpecified=true,sapcRetCode=1,sapcRetCodeSpecified=true},
+                new externalBalanceUpdateResponseActiveDataListDataBalance {dataExpiry="3",    dataValue= 567546,dataValueSpecified=true,sapcRetCode=1,sapcRetCodeSpecified=true},
+                new externalBalanceUpdateResponseActiveDataListDataBalance {dataExpiry="4",    dataValue= 1295876841234,dataValueSpecified=true,sapcRetCode=1,sapcRetCodeSpecified=true},
+            };
+            return retValue;
+        }
+
+        // GET: api/Parameter/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Parameter
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Parameter/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Parameter/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- LIMEWareClient/LIMEWareClient/Scripts/app.js
+++ LIMEWareClient/LIMEWareClient/Scripts/app.js
@@ -1,33 +1,17 @@
 ﻿(function () {
     var app = angular.module('limeware', []);
 
-    app.controller('ParametersController', function () {
-        this.parameters = paramTemplates;
-    });
+    app.controller('ParametersController', ['$scope', '$http', function ($scope, $http) {
+        $scope.parameters = {};
+        $scope.databalance = {};
+        $scope.activeBalanceList = {};
 
-    var paramTemplates = [
-        {
-            msisdn: '0752068078',
-            value: '10',
-            expiry: '5',
-            canShow: true,
-            exists: true,
-        },
-        {
-             msisdn: '0752068078',
-             value: '20',
-             expiry: '5',
-             canShow: true,
-             exists: true,
-        },
-        {
-              msisdn: '0752068078',
-              value: '30',
-              expiry: '5',
-              canShow: true,
-              exists: true,
-        }
+        $scope.add = function () {
 
+            $http.get('api/Parameter').success(function (response) {
+                $scope.databalance = response;
+            });
+        };
+    }]);
 
-    ];
 })();
